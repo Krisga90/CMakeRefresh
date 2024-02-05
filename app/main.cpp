f1b0506415185568861ed081ea42fd5011c62100
krisga90 <krzysztof.meyer@gmail.com>
2024-02-05 22:25:11 +0100

    configured files

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,13 @@
+#include <iostream>
+
 #include "my_lib.hpp"
+#include "config.hpp"
 
 int main(int argc, char** argv)
 {
+
+    std::cout << project_name << "\n";
+    std::cout << project_version << "\n";
     print_hello_world();
 
     return 0;
